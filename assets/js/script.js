8a5008ad9c55756e123eab9d6eb118d539509fb9
komal g <komal@decodershub.com>
2021-03-16 10:22:43 +0530

    in progressdesign

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,83 @@
-
-/*------ fixed-header-js-start ------ */ 
-$(window).on('scroll', function () {
+/*------ fixed-header-js-start ------ */
+$(window).on('scroll', function() {
     if ($(window).scrollTop()) {
         $('.start-header').addClass('sticky-bg');
-    }
-   else {
+    } else {
         $('.start-header').removeClass('sticky-bg');
-   }
- })
+    }
+})
 
 /* ------ dropdown-menu-js-start ------ */
-$(document).ready(function(){
+$(document).ready(function() {
     //the trigger on hover when cursor directed to this class
-      $(".drop-menu li").hover(
-      function(){
-        //i used the parent ul to show submenu
-          $(this).children('ul').slideDown('fast');
-      }, 
+    $(".drop-menu li").hover(
+        function() {
+            //i used the parent ul to show submenu
+            $(this).children('ul').slideDown('fast');
+        },
         //when the cursor away 
-      function () {
-          $('ul', this).slideUp('fast');
-      });
-  });
- 
+        function() {
+            $('ul', this).slideUp('fast');
+        });
+});
+
 /* ------ dropdown-menu-js-end ------ */
-/*------ fixed-header-js-end ------ */ 
+/*------ fixed-header-js-end ------ */
 /*------ CATOGERIES-SLIDER-JS-START -------*/
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
     navText: [
-      "<i class='material-icons'>west</i>",
-      "<i class='material-icons'>east</i>"
+        "<i class='material-icons'>west</i>",
+        "<i class='material-icons'>east</i>"
     ],
     autoplay: true,
     autoplayHoverPause: true,
     responsive: {
-      0: {
-        items: 1
-      },
-      50: {
-        items: 2
-      },
-      1000: {
-        items: 6
-      }
+        0: {
+            items: 1
+        },
+        50: {
+            items: 2
+        },
+        1000: {
+            items: 6
+        }
     }
-  })
+})
 
-  /*------ CATOGERIES-SLIDER-JS-END -------*/
+/*------ CATOGERIES-SLIDER-JS-END -------*/
 
-  /*------ PRODUCTS-DETAIL-JS-START -------*/
-  $(".mini img").click(function(){  
+/*------ PRODUCTS-DETAIL-JS-START -------*/
+$(".mini img").click(function() {
 
-    $(".maxi").attr("src",$(this).attr("src").replace("120x100","500x300"));
-   
-   });
-  /*------ PRODUCTS-DETAIL-JS-END -------*/
+    $(".maxi").attr("src", $(this).attr("src").replace("120x100", "500x300"));
 
-  var unit = 0;
+});
+/*------ PRODUCTS-DETAIL-JS-END -------*/
+/*------ PLUS-MINUS-BUTTON-JS-START -------*/
+var unit = 0;
 var total;
 // if user changes value in field
 $('.field').change(function() {
-  unit = this.value;
+    unit = this.value;
 });
 $('.add').click(function() {
-  unit++;
-  var $input = $(this).prevUntil('.sub');
-  $input.val(unit);
-  unit = unit;
+    unit++;
+    var $input = $(this).prevUntil('.sub');
+    $input.val(unit);
+    unit = unit;
 });
 $('.sub').click(function() {
-  if (unit > 0) {
-    unit--;
-    var $input = $(this).nextUntil('.add');
-    $input.val(unit);
-  }
+    if (unit > 1) {
+        unit--;
+        var $input = $(this).nextUntil('.add');
+        $input.val(unit);
+    }
 });
+
+/*------ PLUS-MINUS-BUTTON-JS-END -------*/
+/*------ SEARCH-BAR-LIST-JS-START -------*/
+
+/*------ SEARCH-BAR-LIST-JS-END -------*/
